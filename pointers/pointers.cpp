20656149ea61aacd1d629761d5ccd59dbd91c796
Toufiqur Rahman <toufiqurraa@gmail.com>
2023-09-11 23:57:55 +0600

    pointer exercises

---
+++ pointers/pointers.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//     float f = 10.5;
+//     float p = 2.5;
+//     float* ptr = &f;
+//     (*ptr)++;   // dereferencing
+//     *ptr = p;
+
+//     cout << *ptr << " " << f << " " << p;
+//     // output: 2.5 2.5 2.5
+
+//     return 0;
+// }
+
+// int main() {
+//     int a = 7;
+//     int b = 17;
+//     int* c = &b;
+//     *c = 7;
+
+//     cout << a << " " << b << endl;
+//     // output: 7 7
+
+//     return 0;
+// }
+
+// int main() {
+//     int* ptr = 0; // null pointer
+//     int a = 10;
+//     *ptr = a;   // deferencing null pointer might cause error
+
+//     cout << *ptr << endl;
+//     // output: error
+
+//     return 0;
+// }
+
+// int main() {
+//     char ch = 'a';
+//     char* ptr = &ch;
+//     ch++;
+
+//     cout << *ptr << endl;
+//     // output: b
+
+//     return 0;
+// }
+
+// int main() {
+//     int a = 7;
+//     int* c = &a;
+//     c = c + 1;
+
+//     cout << a << "  " << *c << endl;
+//     // output: 7 garbage
+
+//     return 0;
+// }
+
+int main() {
+    int a = 7;
+    int* c = &a;
+    c = c + 3;
+
+    cout << &a << "  " << c << endl;
+
+    return 0;
+}
