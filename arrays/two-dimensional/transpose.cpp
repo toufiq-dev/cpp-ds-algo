b68a9b44a7ad6d69b6d39731d4a9ff7ee10dd43d
Toufiqur Rahman <toufiqurraa@gmail.com>
2023-09-11 20:13:27 +0600

    transpose.cpp

---
+++ arrays/two-dimensional/transpose.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> transpose(vector<vector<int>> arr) {
+    int rows = arr.size();
+    int cols = arr[0].size(); 
+
+    vector<vector<int>> ans(rows, vector<int>(cols));
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            ans[j][i] = arr[i][j];
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<vector<int>> arr = {
+        {1, 2, 3},
+        {1, 3, 7},
+        {4, 6, 8}
+    };
+
+    vector<vector<int>> ans = transpose(arr);
+
+    for (int i = 0; i < ans.size(); i++) {
+        for (int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
