2633bb95f7c0c96fb72cab7c3e996bfc0de07f73
toufiq-dev <toufiq.dev@gmail.com>
2023-10-05 07:47:12 +0600

    fix: abstraction-class

--- design-patterns/abstraction-class/bird.h
+++ design-patterns/abstraction-class/bird.h
@@ -4,10 +4,12 @@
 #if !defined(BIRD_H)
 #define BIRD_H
 #include <iostream>
+using namespace std;
 
 class Bird {
-    virtual boid eat() = 0;
-    virtual boid eat() = 0;
+   public:
+        virtual void eat() = 0;
+        virtual void fly() = 0;
     // classes that inherits this class 
     // has to implement pure virtual functions
 };
@@ -15,30 +17,30 @@ class Bird {
 class sparrow : public Bird {
     public: 
         void eat() {
-            std::cout << "Sparrow is eating\n";
+            cout << "Sparrow is eating\n";
         }
         void fly() {
-            std::cout << "Sparrow is flying\n";
+            cout << "Sparrow is flying\n";
         }
 };
 
 class eagle : public Bird {
     public: 
         void eat() {
-            std::cout << "Eagle is eating\n";
+            cout << "Eagle is eating\n";
         }
         void fly() {
-            std::cout << "Eagle is flying\n";
+            cout << "Eagle is flying\n";
         }
 };
 
 class pigeon : public Bird {
     public: 
         void eat() {
-            std::cout << "Eagle is eating\n";
+            std::cout << "Pigeon is eating\n";
         }
         void fly() {
-            std::cout << "Eagle is flying\n";
+            std::cout << "Pigeon is flying\n";
         }
 };
 
--- design-patterns/abstraction-class/main.cpp
+++ design-patterns/abstraction-class/main.cpp
@@ -2,6 +2,15 @@
 #include "bird.h"
 using namespace std;
 
+void birdDoesSomething(Bird *&bird) {
+    bird -> eat();
+    bird -> fly();
+}
+
 int main() {
-    
+    Bird *bird = new pigeon();
+
+    birdDoesSomething(bird);
+
+    return 0;
 }
