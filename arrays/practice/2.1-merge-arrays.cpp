f82d14325491b94cbc97167098746dbf9ec52e3f
toufiq-dev <toufiq.dev@gmail.com>
2023-11-15 19:48:44 +0600

    add merge sorted array and three sum

--- arrays/practice/2.1-merge-arrays.cpp
+++ arrays/practice/2.1-merge-arrays.cpp
@@ -2,32 +2,31 @@
 #include <vector>
 using namespace std;
 
-// TC: O(|nums1| + |nums2|)
+// TC: O(max(|nums1|, |nums2|))
 // SC: constant
 vector<int> mergeArrays(vector<int>& nums1, vector<int>& nums2) {
     vector<int> result;
-    int i = 0;
-    int j = 0;
+    int i = 0, j = 0;
 
     while (i < nums1.size() && j < nums2.size()) {
         if (nums1[i] < nums2[j]) {
             result.push_back(nums1[i]);
-            i++;
+            ++i;
         }
         else {
             result.push_back(nums2[j]);
-            j++;
+            ++j;
         }
     }
 
     // Add remaining elements from nums1 and nums2
     while (i < nums1.size()) {
         result.push_back(nums1[i]);
-        i++;
+        ++i;
     }
     while (j < nums2.size()) {
         result.push_back(nums2[j]);
-        j++;
+        ++j;
     }
 
     return result;
