0a6223e525b7aafa6e7ba2291a39ef3b950631ee
toufiq-dev <toufiq.dev@gmail.com>
2023-12-02 23:57:30 +0600

    fix: hash

--- strings/practice.cpp
+++ strings/practice.cpp
@@ -1,26 +1,54 @@
-#include <iostream>
-#include <string>
+// string hashing
+#include <bits/stdc++.h>
 using namespace std;
 
-bool compare(string &s1, string &s2) {
-    int i = 0, j = 0;
-    while(i < s1.size() || j < s2.size()) {
-        if(s1[i] != s2[j])
-            return false;
-        
-        ++i;
-        ++j;
-    }
+// prefix hash, power of base
+vector<long long> pH, pB;
+const int BASE = 257;
+const int MOD = 1000000007;
 
-    return true;
+/// O(|s|)
+void preprocess(string &s) {
+    pH.resize(s.size() + 1);
+    pB.resize(s.size() + 1);
+    pH[0] = s[0];
+    pB[0] = 1;
+
+    for (int i = 1; i < s.size(); i++) {
+        pH[i] = (pH[i - 1] * BASE) + s[i];
+        if(pH[i] >= MOD)
+            pH[i] %= MOD;
+        
+        pB[i] = pB[i - 1] * BASE; /// BASE^i = BASE^(i - 1) * BASE
+        if(pB[i] >= MOD)
+            pB[i] %= MOD;
+    }
+}
+
+long long getHash(int L, int R) {
+    if(L == 0)
+        return pH[R];
+    
+    long long A = pH[R];
+    long long B = pH[L - 1] * pB[R - L + 1] > MOD 
+                        ? pH[L - 1] * pB[R - L + 1] % MOD 
+                        : pH[L - 1] * pB[R - L + 1];
+                        
+    return (A - B + MOD) % MOD; /// A < B
 }
 
 int main() {
-    string s1 = "Alice";
-    string s2 = "Alicev";
-    bool ans = compare(s1, s2);
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
 
-    cout << ans;
+    preprocess(s);
+
+    while(true) {
+        int L, R;
+        cin >> L >> R;
+        cout << s.substr(L, R - L + 1) << "  " << getHash(L, R) << '\n';
+    }
 
     return 0;
 }
