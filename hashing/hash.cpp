51ec658d6f49f221867dc7e46031d3b8fe03145a
toufiq-dev <toufiq.dev@gmail.com>
2023-12-02 23:57:49 +0600

    fix: hash

--- hashing/hash.cpp
+++ hashing/hash.cpp
@@ -1,53 +1,54 @@
-// String Hashing
+// string hashing
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> pH, po;
+// prefix hash, power of base
+vector<long long> pH, pB;
 const int BASE = 257;
 const int MOD = 1000000007;
 
 /// O(|s|)
-void preprocess(string &s)
-{
+void preprocess(string &s) {
     pH.resize(s.size() + 1);
-    po.resize(s.size() + 1);
+    pB.resize(s.size() + 1);
     pH[0] = s[0];
-    po[0] = 1;
-    for (int i = 1; i < s.size(); i++)
-    {
-        pH[i] = (pH[i - 1] * BASE) + s[i];
-        if (pH[i] >= MOD) pH[i] %= MOD;
+    pB[0] = 1;
 
-        po[i] = (po[i - 1] * BASE); /// BASE^i = BASE^(i - 1) * BASE
-        if (po[i] >= MOD) po[i] %= MOD; /// [0, MOD - 1]
+    for (int i = 1; i < s.size(); i++) {
+        pH[i] = (pH[i - 1] * BASE) + s[i];
+        if(pH[i] >= MOD)
+            pH[i] %= MOD;
+        
+        pB[i] = pB[i - 1] * BASE; /// BASE^i = BASE^(i - 1) * BASE
+        if(pB[i] >= MOD)
+            pB[i] %= MOD;
     }
 }
 
-long long getHash(int L, int R)
-{
-    if (L == 0) return pH[R];
-    /// pH[R] - (pH[L - 1) * po[R - L + 1])
+long long getHash(int L, int R) {
+    if(L == 0)
+        return pH[R];
+    
     long long A = pH[R];
-    long long B = pH[L - 1] * po[R - L + 1] > MOD 
-                        ? pH[L - 1] * po[R - L + 1] % MOD 
-                        : pH[L - 1] * po[R - L + 1];
+    long long B = pH[L - 1] * pB[R - L + 1] > MOD 
+                        ? pH[L - 1] * pB[R - L + 1] % MOD 
+                        : pH[L - 1] * pB[R - L + 1];
                         
     return (A - B + MOD) % MOD; /// A < B
 }
-/// -9, -4, 1, 6, 11, 16, 21, 26, ...
-/// 1, 1, 1, 1, 1, 1, 1, 1, ...
 
-int main()
-{
+int main() {
     string s;
     cout << "Enter a string: ";
     cin >> s;
+
     preprocess(s);
-    while(true)
-    {
+
+    while(true) {
         int L, R;
         cin >> L >> R;
-        cout << s.substr(L, R - L + 1) << " " << getHash(L, R) << "\n";
+        cout << s.substr(L, R - L + 1) << "  " << getHash(L, R) << '\n';
     }
+
     return 0;
 }
