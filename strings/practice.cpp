e32440687d77a7b5dbef51200e70fc90d3b4f79d
toufiq-dev <toufiq.dev@gmail.com>
2024-01-02 23:26:47 +0600

    fix: practice

--- strings/practice.cpp
+++ strings/practice.cpp
@@ -1,33 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> adj;
+vector<int> dist;
+
+void bfs(int src, int n) {
+	dist.clear();
+	dist.resize(n + 1, INT_MAX / 2);
+	queue<int> Q;
+	Q.push(src);
+	dist[src] = 0;
+	
+	while (!Q.empty()) {
+		int u = Q.front(); Q.pop();
+		for (int v : adj[u]) {
+			// if v is visited?
+			if (dist[v] > dist[u] + 1) {
+				dist[v] = dist[u] + 1;
+				Q.push(v);
+			}
+		}
+	}
+}
+
 int main() {
-    vector<string> strs = {"flower", "flow", "flight"};
-
-    string prefix = strs[0]; // prefix = "flower"
-
-    for (auto &str : strs) {
-    // Find the longest common prefix of the current string and the prefix.
-        string new_prefix = "";
-
-        for (int i = 0; i < min(prefix.size(), str.size()); i++) {
-            if (prefix[i] == str[i]) {
-                new_prefix += prefix[i];
-            } else {
-                break;
-            }
-        }
-
-        // Update the prefix with the new common prefix.
-        prefix = new_prefix;
-    }
-
-    // Return the prefix.
-    string longest_common_prefix = prefix;
-
-    cout << longest_common_prefix << endl;
-
-    return 0;
+	int n, m, u, v, src;
+	cin >> n >> m >> src;
+	adj.clear();
+	adj.resize(n + 1, {});
+	for (int i = 0; i < m; i++) {
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	bfs(src, n);
+	for (int i = 1; i <= n; i++) {
+		cout << i << " : " << dist[i] << "\n";
+	}
+	return 0;
 }
